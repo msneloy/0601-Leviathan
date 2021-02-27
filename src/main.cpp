94e74c8a2bda600182bcbc012ef0e47d95d14886
Mahadi Sajjad Neloy <sierra117@qq.com>
2021-02-27 13:04:50 +0800

    comms up

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,35 @@
 #include <Arduino.h>
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "WAC.h"
+const char *ssid = SSID;
+const char *password = PW;
+//Create WAC.h to store creds
+//#define SSID "Wireless Access Point"
+//#define PW "Password"
 
-void setup() {
-  pinMode(LED_BUILTIN,OUTPUT);
+WiFiUDP ntpUDP;
+
+NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", 28800, 60000);
+
+void setup(){
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  timeClient.begin();
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN,HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN,LOW);
+  timeClient.update();
+
+  Serial.println(timeClient.getFormattedTime());
+
   delay(1000);
 }
